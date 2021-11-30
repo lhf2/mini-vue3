baaf72da6973534fda236015398278e5be3cf143
李灏峰 <lihaofeng@promni.cn>
2021-11-30 15:18:35 +0800

    feat：实现处理 element 类型

--- example/helloWorld/App.js
+++ example/helloWorld/App.js
@@ -3,11 +3,17 @@ import {h} from "../../lib/mini-vue3.esm.js";
 export default {
   name: 'App',
   render() {
-    return h('div', this.msg);
+    return h(
+      'div',
+      {
+        id: "root",
+        class: ["red"],
+      },
+      "hello mini-vue3");
   },
   setup() {
     return {
-      msg: 'hello vue3'
+      msg: 'hello mini-vue3'
     }
   }
 }
--- src/runtime-core/renderer.ts
+++ src/runtime-core/renderer.ts
@@ -1,4 +1,5 @@
 import {createComponentInstance, setupComponent} from './components'
+import {isObject} from "../shared/index";
 
 export function render(vnode, container) {
     // 调用patch 根据 vnode 不同类型进行处理
@@ -6,14 +7,20 @@ export function render(vnode, container) {
 }
 
 function patch(vnode, container) {
-    processComponent(vnode, container);
+    // 区分 component 跟 element
+    if (typeof vnode.type === 'string') {
+        processElement(vnode, container)
+
+    } else if (!!isObject(vnode.type)) {
+        processComponent(vnode, container);
+    }
 }
 
 function processComponent(vnode, container) {
-    // 组件初始化
     mountComponent(vnode, container);
 }
 
+// 组件初始化
 function mountComponent(vnode, container) {
     // 1. 创建 component instance 对象
     const instance = createComponentInstance(vnode);
@@ -21,6 +28,36 @@ function mountComponent(vnode, container) {
     setupComponent(instance);
     // 3. setupRenderEffect 调用render函数获取子vnode 递归patch
     setupRenderEffect(instance, container)
+}
+
+function processElement(vnode, container) {
+    mountElement(vnode, container)
+}
+
+function mountElement(vnode, container) {
+    const el = document.createElement(vnode.type);
+    const {props, children} = vnode;
+    // 处理props
+    for (const key in props) {
+        const val = props[key];
+        el.setAttribute(key, val);
+    }
+    // 处理children
+    // string array
+    if (typeof children === 'string') {
+        el.textContent = children
+    } else if (Array.isArray(children)) {
+        // 循环递归调用patch
+        mountChildren(vnode, el);
+    }
+
+    container.append(el);
+}
+
+function mountChildren(vnode, container) {
+    vnode.children.forEach(child => {
+        patch(child, container);
+    })
 }
 
 // 调用render函数拆箱的过程
